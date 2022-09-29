d68e6a6f394fd3f05c8a78d62f8becf163fd9ed1
Na99ets <kirillna99et@gmail.com>
2022-09-29 16:09:41 +0530

    lvl1 Test

---
+++ memories/Assets/lvls&player/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D phisic;
+    private GameObject graundCheck;
+    private bool isGraunded;
+
+    public float graundRadius = 0.3f;
+    public LayerMask graundMask;
+    public float speed = 0.1f;
+    public float jumpforce = 800f;
+
+    void Start()
+    {
+        player = GameObject.Find("player");
+        graundCheck = GameObject.Find("GraundCheck");
+        phisic = player.GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        isGraunded = Physics2D.OverlapCircle(graundCheck.transform.position, graundRadius, graundMask);
+        if(Input.GetKeyDown(KeyCode.Space) && isGraunded)
+        {
+            phisic.AddForce(new Vector3(0, jumpforce, 0));
+        }
+        player.transform.position += new Vector3(speed, 0, 0);
+    }
+}
---
+++ memories/Assets/minu&ui/scripts/startlvl1.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startlvl1 : MonoBehaviour
+{
+    public void StartLvl1()
+    {
+        SceneManager.LoadScene("lvl1");
+    }
+}

fcf03a006961bd64de30c15faa19a9809fac0728
qvarkk <xumdes@gmail.com>
2022-10-05 18:16:33 +0500

    настроил тайлмапы и поменял некоторые префабы

--- memories/Assets/Scripts/PortalBehaviour.cs
+++ memories/Assets/Scripts/PortalBehaviour.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class PortalBehaviour : MonoBehaviour
 {
-    [SerializeField] private Gamemodes gamemode;
+  [SerializeField] private Gamemodes gamemode;
+  [SerializeField] private int state;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    // честно эту хуйню запихнул чисто выебнуться, что выучил, но похуй, заебись смотрится, типа я что-то знаю
+    try
     {
-        // честно эту хуйню запихнул чисто выебнуться, что выучил, но похуй, заебись смотрится, типа я что-то знаю
-        try
-        {
-            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+      PlayerController player = collision.gameObject.GetComponent<PlayerController>();
 
-            player.ChangeThroughPortal(gamemode);
-        }
-        catch {}
+      player.ChangeThroughPortal(gamemode, state);
     }
+    catch { }
+  }
 }

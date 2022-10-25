ad541bb9a91561f7c4b67e637572f4bffc2a9776
qvarkk <xumdes@gmail.com>
2022-10-25 20:38:00 +0500

    куча приколдесов впадлу писать

--- memories/Assets/Scripts/Level Objects/PortalBehaviour.cs
+++ memories/Assets/Scripts/Level Objects/PortalBehaviour.cs
@@ -7,9 +7,8 @@ public class PortalBehaviour : MonoBehaviour
   [SerializeField] private Gamemodes gamemode;
   [SerializeField] private int state;
 
-  private void OnCollisionEnter2D(Collision2D collision)
+  private void OnTriggerEnter2D(Collider2D collision)
   {
-    // честно эту хуйню запихнул чисто выебнуться, что выучил, но похуй, заебись смотрится, типа я что-то знаю
     try
     {
       PlayerController player = collision.gameObject.GetComponent<PlayerController>();
--- memories/Assets/Scripts/PlayerController.cs
+++ memories/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Gamemodes { Cube = 0, QTE = 1 };
+public enum Gamemodes { Cube = 0, QTE = 1, Ship = 2 };
 public class PlayerController : MonoBehaviour
 {
   [SerializeField] private GameObject groundCheck;
@@ -14,21 +14,26 @@ public class PlayerController : MonoBehaviour
   public GameController gameController;
   public Transform sprite;
   public float jumpForce;
+  public float shipForce;
   public float jumpBoostForce;
-  public Rigidbody2D rb;
+  private Rigidbody2D rb;
+  private bool isShip;
+  private float lastKnownHeight = 0;
 
   void Start()
   {
+    lastKnownHeight = gameObject.transform.position.y;
     rb = gameObject.GetComponent<Rigidbody2D>();
   }
 
-  void Update()
+  void FixedUpdate()
   {
-    if (IsGrounded())
+    if (IsGrounded() && !isShip)
     {
       Vector3 spriteRotation = sprite.rotation.eulerAngles;
       spriteRotation.z = Mathf.Round(spriteRotation.z / 90) * 90;
       sprite.rotation = Quaternion.Euler(spriteRotation);
+      particle.gameObject.SetActive(true);
       particle.Play();
 
       if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Mouse0))
@@ -37,11 +42,44 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
       }
     }
+    else if (isShip)
+    {
+      particle.Pause();
+      particle.gameObject.SetActive(false);
+      TiltTheShip();
+      if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Mouse0))
+      {
+        rb.velocity = Vector2.zero;
+        rb.AddForce(Vector2.up * shipForce, ForceMode2D.Impulse);
+      }
+      if (IsGrounded())
+      {
+        Vector3 spriteRotation = sprite.rotation.eulerAngles;
+        spriteRotation.z = Mathf.Round(spriteRotation.z / 90) * 90;
+        sprite.rotation = Quaternion.Euler(spriteRotation);
+      }
+    }
     else
     {
-      sprite.Rotate(new Vector3(0, 0, -1));
-      particle.Stop();
+      sprite.Rotate(new Vector3(0, 0, -5));
+      particle.Pause();
+      particle.gameObject.SetActive(false);
     }
+  }
+
+
+  private void TiltTheShip()
+  {
+    if (lastKnownHeight > gameObject.transform.position.y && sprite.rotation.z > -0.34)
+    {
+      sprite.Rotate(new Vector3(0, 0, -3.69f));
+    }
+    else if (lastKnownHeight < gameObject.transform.position.y && sprite.rotation.z < 0.34)
+    {
+      sprite.Rotate(new Vector3(0, 0, +1.75f));
+    }
+
+    lastKnownHeight = gameObject.transform.position.y;
   }
 
   private bool IsGrounded()
@@ -89,10 +127,15 @@ public class PlayerController : MonoBehaviour
       case 0:
         rb.gravityScale = 10;
         currentGamemode = gamemode;
+        isShip = false;
         break;
       case 1:
         rb.velocity = Vector2.zero;
         rb.gravityScale = 0;
+        break;
+      case 2:
+        sprite.rotation = Quaternion.identity;
+        isShip = true;
         break;
     }
   }

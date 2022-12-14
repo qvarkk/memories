a6990d61ad6e9fcde408918799a0454cb461912a
Na99ets <kirillna99et@gmail.com>
2022-12-14 19:45:25 +0530

    шик лоск

---
+++ memories/Assets/Scripts/Pause.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject pauseButton;
+    [SerializeField] private AudioSource zvuk;
+
+
+
+    private static bool gameIsPaused = false;
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(gameIsPaused){
+                Resume();
+            }
+            else{
+                Pause_();
+            }
+        }
+    }
+
+    public void Resume(){
+        pauseMenu.SetActive(false);
+        pauseButton.SetActive(true);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+        zvuk.UnPause();
+    }
+
+    public void Pause_(){
+        pauseMenu.SetActive(true);
+        pauseButton.SetActive(false);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+        zvuk.Pause();
+    }
+}
